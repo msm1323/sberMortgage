8cc21bf002c082622a5b105a7eee0cc7735265cb
MagomedovaSvetlana13 <svetlana.magomedova.1998@gmail.com>
2021-10-14 12:28:03 +0300

    Added logging (level "info")

--- src/main/java/ru/msm/framework/steps/CreditsCompleteHousePageStep.java
+++ src/main/java/ru/msm/framework/steps/CreditsCompleteHousePageStep.java
@@ -3,35 +3,40 @@ package ru.msm.framework.steps;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.ru.И;
 import io.cucumber.java.ru.Тогда;
-import io.qameta.allure.Allure;
-import io.qameta.allure.cucumber5jvm.AllureCucumber5Jvm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.msm.framework.managers.PageManager;
-
-import java.io.InputStream;
-import java.util.stream.Stream;
 
 public class CreditsCompleteHousePageStep {
 
     PageManager PAGE_MANAGER = PageManager.getINSTANCE();
 
+    private static final Logger logger = LoggerFactory.getLogger(CreditsCompleteHousePageStep.class);
+
     @Тогда("^заполнить поля$")
     public void fillFields(DataTable arg) {
         PAGE_MANAGER.getCreditsCompleteHousePage().fillFields(arg.asLists());
+        logger.info("Поля \"{}\", \"{}\" и \"{}\" заполнены",
+                arg.asLists().get(0).get(0), arg.asLists().get(1).get(0), arg.asLists().get(2).get(0));
     }
 
     @И("^убрать галочку \"(.+)\"$")
     public void unselect(String arg0) {
         PAGE_MANAGER.getCreditsCompleteHousePage().switcherDiscountsBlock(false, arg0);
+        logger.info("Выключен чекбокс \"{}\"", arg0);
     }
 
     @И("^поставить галочку \"(.+)\"$")
     public void select(String arg0) {
         PAGE_MANAGER.getCreditsCompleteHousePage().switcherDiscountsBlock(true, arg0);
+        logger.info("Включен чекбокс \"{}\"", arg0);
     }
 
     @Тогда("проверить значения полей")
     public void checkLabelsValues(DataTable arg) {
         PAGE_MANAGER.getCreditsCompleteHousePage().checkLabelsValues(arg.asLists());
+        logger.info("Поля \"{}\", \"{}\", \"{}\" и \"{}\" проверены", arg.asLists().get(0).get(0),
+                arg.asLists().get(1).get(0), arg.asLists().get(2).get(0), arg.asLists().get(3).get(0));
     }
 
 }
--- src/main/java/ru/msm/framework/steps/StartPageStep.java
+++ src/main/java/ru/msm/framework/steps/StartPageStep.java
@@ -2,19 +2,25 @@ package ru.msm.framework.steps;
 
 import io.cucumber.java.ru.И;
 import io.cucumber.java.ru.Тогда;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.msm.framework.managers.PageManager;
 
 public class StartPageStep {
 
     PageManager PAGE_MANAGER = PageManager.getINSTANCE();
 
+    private static final Logger logger = LoggerFactory.getLogger(StartPageStep.class);
+
     @Тогда("^перейти на сайт https://www.sberbank.ru/ru/person и в верхнем меню нажать на \"(.+)\"$")
     public void clickOnTopMenuIcon(String icon) {
         PAGE_MANAGER.getStartPage().closeCookiesDialog().clickOnTopMenuIcon(icon);
+        logger.info("На стартовой странице нажата иконка \"{}\"", icon);
     }
 
     @И("^дождаться открытия выпадающего меню и выбрать \"(.+)\"$")
     public void waitDropdownMenu(String subIcon) {
         PAGE_MANAGER.getStartPage().waitDropdownMenu(subIcon);
+        logger.info("В выпадающем меню выбрано \"{}\"", subIcon);
     }
 }
